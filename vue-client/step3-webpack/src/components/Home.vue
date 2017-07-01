7629734dc3797366e9df44240e5fbc256474577b
Rob Archibald <rob.archibald@endfirst.com>
2017-07-01 14:57:33 -0700

    Update to use structure we'll use for FB Clone

---
+++ vue-client/step3-webpack/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Hello {{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'World',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
