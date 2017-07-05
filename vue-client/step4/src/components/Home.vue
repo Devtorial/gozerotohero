80977313585b6397741d8ff9485229311cb1b390
Rob Archibald <rob.archibald@endfirst.com>
2017-07-05 10:12:30 -0700

    Fix property passing of user

--- vue-client/step4/src/components/Home.vue
+++ vue-client/step4/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <navbar></navbar>
+    <navbar :me="me"></navbar>
     <div class="page">
       <div id="leftnav">
         <ul v-if="me">
--- vue-client/step4/src/router/index.js
+++ vue-client/step4/src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    { path: '/', component: Home, props: { me: this.me } },
+    { path: '/', component: Home, props: true },
   ],
   props: ['me'],
 });
